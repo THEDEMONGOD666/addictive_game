cb2d57cd718965fe75b452f442dc61cdedea26c1
THEDEMONGOD666 <abhisaini1098@gmail.com>
2026-05-29 12:39:31 +1200

    player movements

--- script/user.gd
+++ script/user.gd
@@ -1,11 +1,52 @@
 extends CharacterBody2D
 
+@export var speed: float = 200.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+enum State { NORMAL, ATTACKING, HIT }
+var current_state: State = State.NORMAL
 
+func _physics_process(delta: float) -> void:
+	match current_state:
+		State.NORMAL:
+			handle_movement_input()
+			handle_animation_normal()
+		State.ATTACKING:
+			velocity = Vector2.ZERO 
+		State.HIT:
+			velocity = Vector2.ZERO 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	move_and_slide()
+	handle_sprite_direction()
+
+func handle_movement_input() -> void:
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_vector.normalized() * speed
+
+	if Input.is_action_just_pressed("attack"):
+		current_state = State.ATTACKING
+		animated_sprite.play("attack")
+		print("yo")
+
+func handle_animation_normal() -> void:
+	if velocity != Vector2.ZERO:
+		animated_sprite.play("walk")
+		print("yay")
+	else:
+		animated_sprite.play("idle_1")
+		print("hi")
+	if not State.ATTACKING and State.HIT:
+		if velocity != Vector2.ZERO:
+			animated_sprite.play("walk")
+		else:
+			animated_sprite.play("idle_1")
+
+func handle_sprite_direction() -> void:
+	if velocity.x < 0:
+		animated_sprite.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite.flip_h = false
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "attack" or animated_sprite.animation == "get-hit":
+		current_state = State.NORMAL 
