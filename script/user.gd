7e7a1f0428f6424c313b184c61b4691da1fa2db0
THEDEMONGOD666 <abhisaini1098@gmail.com>
2026-06-08 14:16:21 +1200

    sprint 1 done

--- script/user.gd
+++ script/user.gd
@@ -12,41 +12,36 @@ func _physics_process(delta: float) -> void:
 			handle_movement_input()
 			handle_animation_normal()
 		State.ATTACKING:
-			velocity = Vector2.ZERO 
+			velocity = Vector2.ZERO
 		State.HIT:
-			velocity = Vector2.ZERO 
-
+			velocity = Vector2.ZERO
 	move_and_slide()
 	handle_sprite_direction()
 
 func handle_movement_input() -> void:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_vector.normalized() * speed
-
 	if Input.is_action_just_pressed("attack"):
 		current_state = State.ATTACKING
+		velocity = Vector2.ZERO
+		animated_sprite.stop()
 		animated_sprite.play("attack")
-		print("yo")
 
 func handle_animation_normal() -> void:
+	if current_state != State.NORMAL:
+		return
 	if velocity != Vector2.ZERO:
 		animated_sprite.play("walk")
-		print("yay")
 	else:
 		animated_sprite.play("idle_1")
-		print("hi")
-	if not State.ATTACKING and State.HIT:
-		if velocity != Vector2.ZERO:
-			animated_sprite.play("walk")
-		else:
-			animated_sprite.play("idle_1")
 
 func handle_sprite_direction() -> void:
-	if velocity.x < 0:
+	var mouse_pos = get_global_mouse_position()
+	if mouse_pos.x < global_position.x:
 		animated_sprite.flip_h = true
-	elif velocity.x > 0:
+	else:
 		animated_sprite.flip_h = false
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite.animation == "attack" or animated_sprite.animation == "get-hit":
-		current_state = State.NORMAL 
+		current_state = State.NORMAL
